60f8165ef42aceeeafa53ab3c17e69feca40706e
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-16 12:52:48 -0500

    feat: slide onboarding from left

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,10 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="onboarding"
+        options={{ headerShown: false, animation: "slide_from_left" }}
+      />
       <Stack.Screen
         name="new"
         options={{
